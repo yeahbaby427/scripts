71a826b6cac7537b53a532888985bf5ddfee0374
yeahbaby427 <63921579+yeahbaby427@users.noreply.github.com>
2023-08-09 16:22:40 +0800

    Update setu.js

--- setu.js
+++ setu.js
@@ -9,7 +9,7 @@
 // @ScriptURL         https://github.com/yeahbaby427/scripts/blob/master/setu.js
 // ==/UserScript==
 
-let userApikey   = '6558307060:AAFSrsAX6SlA5JJUc04UWJlxBFAVfdasMjI' //请在 Telegram 内使用 @loliconApiBot 申请
+let userApikey   = "6558307060:AAFSrsAX6SlA5JJUc04UWJlxBFAVfdasMjI" //请在 Telegram 内使用 @loliconApiBot 申请
 let userR18      = 2 //18禁为1 非为0 2是混合
 let userKeyword  = ''//搜索关键字
 let userNum      = 1 //一次返回的结果数量，范围为1到10，数字的数量亦为弹框的次数
